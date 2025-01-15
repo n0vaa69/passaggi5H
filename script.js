5f7b46f160fe63c419df0391d3e4b9d60e9b5f1c
n0vaa69 <florea.emanuel62@gmail.com>
2025-01-15 22:47:33 +0100

    codice funzionante

--- script.js
+++ script.js
@@ -1,30 +1,39 @@
-
-function creaCard(linea, orario){
+function creaCard(linea, orario) {
     let riga = document.getElementById("riga");
-    let div = document.createElement("div");   
-    let p1 = document.createElement("p");   
-    let p2 = document.createElement("p");
-    p1.innerHTML = linea;
-    p2.innerHTML = orario;
-    p1.classList.add("text-bg-primary");
-    div.appendChild(p1);
-    div.appendChild(p2);
+    let div = document.createElement("div");
+    div.classList.add("card");
+
+    let divBody = document.createElement("div");
+    divBody.classList.add("card-body");
+
+    let divLinea = document.createElement("div");
+    divLinea.classList.add("linea");
+    divLinea.innerHTML = "Linea: " + linea;
+
+    let p1 = document.createElement("p");
+    p1.innerHTML = "Orario: " + orario;
+    p1.classList.add("card-text");
+
+    divBody.appendChild(divLinea); 
+    divBody.appendChild(p1);  
+    div.appendChild(divBody);
     riga.appendChild(div);
 }
 
-
-function cerca(){
-    let numeroFermata =  document.getElementById("input").value;
+function cerca() {
+    let numeroFermata = document.getElementById("input").value;
 
     let URL = "https://gpa.madbob.org/query.php?stop=" + numeroFermata;
 
     fetch(URL)
-    .then(x => x.json())
-    .then(y =>{
-        creaCard(y[0].line, y[0].hour);
-        creaCard(y[1].line, y[1].hour);
-        creaCard(y[2].line, y[2].hour);
-    });
-    
+        .then(x => x.json())
+        .then(y => {
+            document.getElementById("riga").innerHTML = "";
+            y.forEach(item => {
+                creaCard(item.line, item.hour);
+            });
+        })
+        .catch(error => {
+            alert("Errore nel recupero dei dati. Riprova.");
+        });
 }
-
